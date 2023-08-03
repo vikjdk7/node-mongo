12055b31511e13222b06593aa33e31235003c73c
Vikash Bharti <mmatenge@vigilan.co.bw>
2023-08-04 00:21:08 +0530

    use of copilot

--- routes/usersrouter.js
+++ routes/usersrouter.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/usercontroller');
-const validator = require('../middlewares/validators')
+const userController = require("../controllers/usercontroller");
+const validator = require("../middlewares/validators");
 
-router.get('/', userController.getAllUsers);
-router.get('/:id', userController.getUserById);
-router.post('/', validator.validateUser, userController.createUser);
-router.put('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+router.get("/", userController.getAllUsers);
+router.get("/:id", userController.getUserById);
+router.post("/", validator.validateUser, userController.createUser);
+router.put("/:id", userController.updateUser);
+router.delete("/:id", userController.deleteUser);
 
 module.exports = router;
